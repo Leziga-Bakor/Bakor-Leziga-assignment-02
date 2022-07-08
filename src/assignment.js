ea5a9a9e9b1c51721fee0e031aebf1daf45179a2
Leziga Bakor <leziga.bakor@gmail.com>
2022-07-08 15:31:51 +0100

    Added solution to question 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+
+    for (let num of arrayOfNumbers) {
+        sum = sum + num;
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
